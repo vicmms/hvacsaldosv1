6ec1f5644448caf1275897e0aaeb334bc55af230
vicmms <victormmsauz@gmail.com>
2021-11-05 08:45:37 -0600

    Brands by category

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex items-center">
             <h2 class="font-semibold text-xl text-gray-600 leading-tight">
-                Lista de productos {{ Auth::user()->getRoleNames() }}
+                Lista de productos
             </h2>
 
             <x-button-enlace class="ml-auto" href="{{ route('admin.products.create') }}">
