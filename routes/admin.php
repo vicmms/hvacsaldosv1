7efa027c51a8a37c62bc5bf2e3796e723de8ed89
vicmms <victormmsauz@gmail.com>
2021-11-24 17:39:13 -0600

    vista para todas las notificaciones

--- routes/admin.php
+++ routes/admin.php
@@ -17,6 +17,7 @@ use App\Http\Livewire\Admin\BrandComponent;
 use App\Http\Livewire\Admin\DepartmentComponent;
 use App\Http\Livewire\Admin\ShowDepartment;
 use App\Http\Livewire\Admin\CityComponent;
+use App\Http\Livewire\Admin\Notifications;
 use App\Http\Livewire\Admin\UserComponent;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
@@ -38,5 +39,7 @@ Route::get('departments', DepartmentComponent::class)->name('admin.departments.i
 Route::get('departments/{department}', ShowDepartment::class)->name('admin.departments.show');
 
 Route::get('cities/{city}', CityComponent::class)->name('admin.cities.show');
+
+Route::get('notifications', Notifications::class)->name('admin.notifications');
 
 Route::get('users', UserComponent::class)->name('admin.users.index');
