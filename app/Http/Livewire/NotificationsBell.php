9c5d79e536d138c785f841783973b806e0e9bcfb
vicmms <victormmsauz@gmail.com>
2021-12-01 17:34:13 -0600

    fix notifications product null

--- app/Http/Livewire/NotificationsBell.php
+++ app/Http/Livewire/NotificationsBell.php
@@ -11,39 +11,39 @@ class NotificationsBell extends Component
 {
     protected $listeners = ['newBellNotification'];
 
-    public function newBellNotification(){
+    public function newBellNotification()
+    {
         $this->render();
     }
 
-    public function clearNotifications(){
+    public function clearNotifications()
+    {
         Notification::where('user_id', Auth::user()->id)
-                                    ->where('read', false)
-                                    ->update(['read' => true]);
+            ->where('read', false)
+            ->update(['read' => true]);
     }
 
     public function render()
     {
         $notifications_count = Notification::where('user_id', Auth::user()->id)
-                                    ->where('read', false)
-                                    ->count();
-        
+            ->where('read', false)
+            ->count();
+
         $notifications = Notification::where('user_id', Auth::user()->id)
-                                    ->orderBy('notifications.created_at', 'desc')
-                                    ->limit(10)
-                                    ->get();
+            ->orderBy('notifications.created_at', 'desc')
+            ->limit(10)
+            ->get();
         foreach ($notifications as $notification) {
-            if($notification->product_id){
-                $product = Product::where('id', $notification->product_id)->first();
-                $notification->image_url = null;
+            $product = Product::where('id', $notification->product_id)->first();
+            $notification->image_url = null;
+            $notification->product_name = null;
+            $notification->product_slug = null;
+            if ($notification->product_id) {
                 if ($product) {
                     $notification->image_url = $product->images()->count() ? $product->images()->first()->url : 'images/image-not-found.png';
+                    $notification->product_name = $product->name;
+                    $notification->product_slug = $product->slug;
                 }
-                $notification->product_name = $product->name;
-                $notification->product_slug = $product->slug;
-            }else{
-                $notification->image_url = null;
-                $notification->product_name = null;
-                $notification->product_slug = null;
             }
         }
         $notifications->number = $notifications_count;
