d566af84f1b7a6e25ac02b701c48cd925ca128c5
vicmms <victormmsauz@gmail.com>
2021-12-09 10:35:52 -0600

    send email verification in app

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
-
+        $user->sendEmailVerificationNotification();
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer'
