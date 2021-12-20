1350a6722af54349beeae853e7042e7cf0d789b5
vicmms <victormmsauz@gmail.com>
2021-12-20 17:55:49 -0600

    Update show-product.blade.php

--- resources/views/livewire/show-product.blade.php
+++ resources/views/livewire/show-product.blade.php
@@ -33,7 +33,7 @@
                 <h1 class="text-3xl font-bold text-trueGray-700">{{ $product->name }} {!! $product->isOffer ? '<span class="px-2 py-1 text-xs bg-red-500 text-white rounded-3xl align-middle"><i class="fas fa-fire-alt"></i> Oferta</span>' : '' !!}</h1>
                 <p class="text-trueGray-700 font-semibold">Categoría: {{ $product->category->name }}</p>
 
-                <p class="text-trueGray-700 font-semibold capitalize">Subcategoría: <span class="text-green-600">{{ $product->subcategory->name }}</span>
+                <p class="text-trueGray-700 font-semibold capitalize">Subcategoría: <span class="text-green-600 font-bold">{{ $product->subcategory->name }}</span>
                 </p>
 
                 <p class="text-trueGray-700 font-semibold capitalize">Marca: {{ $product->brand->name }}</p>
