87663819744002448cc71975d97ae5457d4ec7bf
vicmms <victormmsauz@gmail.com>
2022-01-03 11:37:32 -0600

    category-filter responsive

--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -1,4 +1,7 @@
 <div>
+    <style>
+        .wrapper{height: 50px;}
+    </style>
     <div class="bg-white rounded-lg shadow-lg mb-6">
         <div class="px-6 py-2 flex justify-between items-center">
             <h1 class="font-semibold text-gray-700 uppercase">{!! $category == 'all' ? 'Todos los productos' : ($category == 'offers' ? '<i class="fas fa-fire-alt"></i> Ofertas' : $category->name) !!}</h1>
@@ -17,7 +20,8 @@
         <aside>
 
             <h2 class="font-semibold text-center mb-2">Subcategorías</h2>
-            <ul class="divide-y divide-gray-200">
+            {{-- computadora --}}
+            <ul class="divide-y divide-gray-200 hidden sm:block">
                 <li class="py-2 text-sm">
                     <a class="cursor-pointer hover:text-orange-500 capitalize {{ !$subcategoria ? 'text-orange-500 font-semibold' : '' }}"
                         wire:click="limpiarSubcategories">Todas
@@ -31,9 +35,21 @@
                     </li>
                 @endforeach
             </ul>
+            {{-- movil --}}
+            <div class="w-full text-center">
+                <select class="sm:hidden form-control w-80" wire:model="subcategoria">
+                    <option value="">Todas</option>
+                    @foreach ($subcategories as $subcategory)
+                        <option class="py-2 text-sm"value="{{ $subcategory->slug }}">
+                            {{ $subcategory->name }}
+                        </option>
+                    @endforeach
+                <select>
+            </div>
 
             <h2 class="font-semibold text-center mt-4 mb-2">Marcas</h2>
-            <ul class="divide-y divide-gray-200 max-h-96 overflow-y-scroll">
+            {{-- computadora --}}
+            <ul class="divide-y divide-gray-200 max-h-96 overflow-y-scroll hidden md:block">
                 <li class="py-2 text-sm">
                     <a class="cursor-pointer hover:text-orange-500 capitalize {{ !$marca ? 'text-orange-500 font-semibold' : '' }}"
                         wire:click="limpiarBrands">
@@ -49,10 +65,23 @@
                     </li>
                 @endforeach
             </ul>
+            {{-- movil --}}
+            <div class="w-full text-center wrapper">
+                <select class="sm:hidden form-control w-80" wire:model="marca" onfocus='this.size=7;' onblur='this.size=1;' onchange='this.size=1; this.blur();'>
+                    <option value="">Todas </option>
+                    @foreach ($brands as $brand)
+                        <option class="py-2 text-sm" value="{{ $brand->name }}">
+                            {{ $brand->name }}
+                        </option>
+                    @endforeach
+                <select>
+            </div>
 
-            <x-jet-button class="mt-4" wire:click="limpiar">
-                Eliminar filtros
-            </x-jet-button>
+            <div class="w-full text-center">
+                <x-jet-button class="mt-4" wire:click="limpiar">
+                    Eliminar filtros
+                </x-jet-button>
+            </div>
         </aside>
 
         <div class="md:col-span-2 lg:col-span-4">
@@ -108,7 +137,7 @@
 
                     @empty
 
-                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded md:relative"
                             role="alert">
                             <strong class="font-bold">Upss!</strong>
                             <span class="block sm:inline">No se han encontrado productos.</span>
